755092c9783930684f8374c97550312ef62b2ef1
vikrant912 <vikrantmohite68@gmail.com>
2026-03-22 14:36:35 +0530

    update5

--- basic_obejct.py
+++ basic_obejct.py
@@ -4,3 +4,16 @@ a[1]=100 #this is assignment of obejct 100 in place of 1
 print(a)
 result=a[1:3] #this is sclice 
 print(result)
+
+
+#####tuple has ( ) brackets values cannot be change like list 
+c=(10,20,30,40) #this is tuple 
+#c[2]=300  # this we tried for testing will give error 
+
+print(c)
+
+#set which does not hold any duplicate value 
+
+d={1,2,3,4,55,55} #this is set {}
+
+print(d)
