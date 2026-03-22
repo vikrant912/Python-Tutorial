8cb980efc739b770da5b33ca337d9ddc7d7ae155
vikrant912 <vikrantmohite68@gmail.com>
2026-03-22 13:45:45 +0530

    update5

---
+++ basic_obejct.py
@@ -0,0 +1,6 @@
+a=[5,4,7,9,11,12,9] #list will start from 0,1,2,3,4,5 value
+a[2]=a[3] #this is assignment of obejct 3 in place of 2 
+a[1]=100 #this is assignment of obejct 100 in place of 1
+print(a)
+result=a[1:3] #this is sclice 
+print(result)
