345ca66bad210ade1fb7242fde08a72450143c63
vikrant912 <vikrantmohite68@gmail.com>
2026-03-23 10:01:04 +0530

    update11

--- module_tutorial.py
+++ module_tutorial.py
@@ -1,5 +1,16 @@
-import datetime
+# import datetime
 
-currentdate= datetime.datetime.now()
+# currentdate= datetime.datetime.now()
+
+# print(currentdate)
+
+#above is one way of doing things but prefer following method
+
+from datetime import datetime
+
+currentdate= datetime.now()
 
 print(currentdate)
+
+
+
