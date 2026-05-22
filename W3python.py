dd52369118dd6f15437314e55e7769e1d6f08168
Vikrant Mohite <vikrant.mohite@aithonsolutions.com>
2026-05-22 13:20:43 +0530

    220626

--- W3python.py
+++ W3python.py
@@ -88,7 +88,34 @@
 
 # print(1 < x and x < 10)
 # x = 5
-hello world 
-and 	Returns True if both statements are true	x < 5 and  x < 10	
-or	Returns True if one of the statements is true	x < 5 or x < 4	
-not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+# hello world 
+# and 	Returns True if both statements are true	x < 5 and  x < 10	
+# or	Returns True if one of the statements is true	x < 5 or x < 4	
+# not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+# sum1 = 100 + 50      # 150 (100 + 50)
+# sum2 = sum1 + 250    # 400 (150 + 250)
+# sum3 = sum2 + sum2   # 800 (400 + 400)
+# print(sum1)
+# print(sum2)
+# print(sum3)
+# x = 15
+# y = 4
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y)
+# print(x ** y)
+# # print(x // y)
+# numbers = [1, 2, 3, 4, 5]
+
+# if (count := len(numbers)) > 3:
+#     print(f"List has {count} elements")
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
